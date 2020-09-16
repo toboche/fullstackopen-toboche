fc94179c6b60115e79bd1272de0157a7d4540fc4
toboche <toboche@gmail.com>
2020-09-16 21:56:54 +0200

    1.7 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
+import { findAllByText } from '@testing-library/react'
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const allCount = good+bad+neutral
   return (
     <div>
       <h2>give feedback</h2>
@@ -17,6 +18,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {allCount}</div>
+      <div>average {(good-bad)/allCount}</div>
+      <div>positive {good/allCount*100}%</div>
     </div>
   )
 }
