779f5d68ff892becdeb3a500a3563e17e7ef4b72
toboche <toboche@gmail.com>
2020-09-16 21:47:20 +0200

    1.6 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,7 +9,14 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h2>give feedback</h2>
+      <button onClick={() => setGood(good+1)}>good</button>
+      <button onClick={() => setNeutral(neutral+1)}>neutral</button>
+      <button onClick={() => setBad(bad+1)}>bad</button>
+      <h2>statistics</h2>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
     </div>
   )
 }
