5eee7e3c5319351780d0e2031628d40836398eb7
toboche <toboche@gmail.com>
2020-09-18 20:10:35 +0200

    1.14

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -4,15 +4,47 @@ import ReactDOM from 'react-dom'
 const App = (props) => {
   const [selected, setSelected] = useState(0)
   const next = Math.floor(Math.random()*(anecdotes.length))
+  const [votes, setVotes] = useState(Array.from({length: anecdotes.length}, (x,i) => 0))
   console.log(next);
+  const incrementVotes = () => {
+    const newPoints = [...votes]
+    newPoints[selected] += 1
+    setVotes(newPoints)
+  }
+
+  const max = Math.max(...votes)
+
+  console.log(votes);
+  const maxVotesIndex = votes.findIndex((current) => current === max)
+  console.log(maxVotesIndex);
+  const maxVotes = votes[maxVotesIndex]
+  const maxVotesAnecdote = anecdotes[maxVotesIndex]
 
   return (
     <div>
+      <h2>
+        Anecdote of the day
+      </h2>
       <div>
         {props.anecdotes[selected]}
       </div>
       <div>
+        has {votes[selected]} votes
+      </div>
+      <div>
         <button onClick={() => setSelected(next)}>next anecdote</button>
+        <button onClick={() => incrementVotes()}>vote</button>
+      </div>
+      <h2>
+        Anecdote with most votes
+      </h2>
+      <div>
+      <div>
+        {maxVotesAnecdote}
+      </div>
+      <div>
+        has {maxVotes} votes
+      </div>
       </div>
     </div>
   )
