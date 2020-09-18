0bd70d5c7d088ab69f2d9ca90ba22b908f5c5264
toboche <toboche@gmail.com>
2020-09-18 19:08:43 +0200

    1.10

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,21 +2,29 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import { findAllByText } from '@testing-library/react'
 
+const Statistic = (props) => (
+  <div>{props.text}</div>
+)
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+)
+
 const Statistics = (props) => {
   if(props.allCount===0)
     return (
-      <div>No feedback given</div>
+      <div>No  feedback given</div>
     )
   else
     return (
       <div>
         <h2>statistics</h2>
-        <div>good {props.good}</div>
-        <div>neutral {props.neutral}</div>
-        <div>bad {props.bad}</div>
-        <div>all {props.allCount}</div>
-        <div>average {(props.good - props.bad) / props.allCount}</div>
-        <div>positive {props.good / props.allCount * 100}%</div>
+        <Statistic text={'good ' + props.good} />
+        <Statistic text={'neutral ' + props.neutral}/>
+        <Statistic text={'bad '+ props.bad}/>
+        <Statistic text={'all ' + props.allCount}/>
+        <Statistic text={'average ' + (props.good - props.bad) / props.allCount}/>
+        <Statistic text={'positive ' + props.good / props.allCount * 100 +'%'}/>
       </div>
     )
 }
@@ -30,9 +38,9 @@ const App = () => {
   return (
     <div>
       <h2>give feedback</h2>
-      <button onClick={() => setGood(good+1)}>good</button>
-      <button onClick={() => setNeutral(neutral+1)}>neutral</button>
-      <button onClick={() => setBad(bad+1)}>bad</button>
+      <Button handleClick={() => setGood(good+1)} text='good'/>
+      <Button handleClick={() => setNeutral(neutral+1)} text='neutral'/>
+      <Button handleClick={() => setBad(bad+1)} text='bad'/>
       <Statistics good={good} 
       bad={bad} 
       neutral={neutral}
