48eb8700f33096bc929e97a47142f90e00facd44
toboche <toboche@gmail.com>
2020-09-20 20:25:15 +0200

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { 
-      name: 'Arto Hellas',
-      phone: '123123'
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [filter, setFilter] = useState('')
 
   const onNameSubmit = (event) => {
     event.preventDefault()
@@ -27,9 +28,14 @@ const App = () => {
     }
   }
 
+  const filteredPersons = persons
+    .filter(person => person.name.includes(filter))
   return (
     <div>
       <h2>Phonebook</h2>
+      <div >
+        filter shown with <input value={filter} onChange={(event) => setFilter(event.target.value)}/>
+      </div>
       <form onSubmit={onNameSubmit}>
         <div>
           name: <input key='name' value={newName} onChange={(event) => setNewName(event.target.value)}/>
@@ -43,7 +49,7 @@ const App = () => {
       </form>
       <div>debug: {newName}</div>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name} {person.phone}</div>)}
+      {filteredPersons.map(person => <div key={person.name}>{person.name} {person.phone}</div>)}
     </div>
   )
 }
