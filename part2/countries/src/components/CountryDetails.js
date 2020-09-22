82bc50ce0cbc234fa2781c8c0e9c4b3d1f2606be
toboche <toboche@gmail.com>
2020-09-22 22:09:17 +0200

    2.14

--- part2/countries/src/components/CountryDetails.js
+++ part2/countries/src/components/CountryDetails.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
-const CountryDetails = ({country}) => (
-    <div>
+const CountryDetails = ({country}) => {
+    const [weatherData, setWeatherData] = useState(null)
+    useEffect(() =>{
+        const api_key = process.env.REACT_APP_API_KEY
+        const url = `http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`
+        axios
+            .get(url)
+            .then(result =>{
+                setWeatherData(result.data)
+            })
+    }, [])
+
+    return <div>
         <h2>{country.name}</h2>
         <div>capital {country.capital}</div>
         <div>popoulation {country.population}</div>
@@ -10,7 +22,17 @@ const CountryDetails = ({country}) => (
             {country.languages.map(language => <li key={language.name}>{language.name}</li>)}
         </ul>
         <img src={country.flag} />
+        {weatherData===null
+            ?<div></div>
+            :<div>
+                <h3>weather in {country.capital}</h3>
+                <div><b>temperature: </b> {weatherData.current.temperature}</div>
+                <img src={weatherData.current.weather_icons[0]}></img>
+                <div><b>wind: </b>{weatherData.current.wind_speed} mph direction {weatherData.current.wind_dir}</div>
+            </div>
+    }
+        
     </div>
-)
+}
 
 export default CountryDetails
